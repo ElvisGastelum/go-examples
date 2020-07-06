f253c36e8afe25b91d4c2bd2fde7430c676e83c6
Elvis Gastelum <elvisgastelum@outlook.com>
2020-07-06 12:14:00 -0700

    [add] request log

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,6 +1,7 @@
 package webserver
 
 import (
+	"log"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,10 +11,11 @@ import (
 // Serve is for init the simple hello world in a web server
 func Serve(port string){
 	http.HandleFunc("/", handleHelloWorld)
+	log.Printf("Listen on port %s", port)
 	http.ListenAndServe(port, nil)
 }
 
-
 func handleHelloWorld(w http.ResponseWriter, r *http.Request)  {
+	log.Printf("%v, Request to: %v, Host: %v, Remote Address: %v", r.Method, r.RequestURI, r.Host, r.RemoteAddr)
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
